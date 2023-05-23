ea2d7ac29327475a7a579d86111fb439a0c1ffd6
Omar Mashaal <omar@teacups.io>
2023-05-24 06:04:59 +1000

    fmt

--- src/main.rs
+++ src/main.rs
@@ -7,48 +7,48 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
+    let mut headers = HeaderMap::new();
+    headers.insert("cache-control", HeaderValue::from_static("no-cache"));
+    let headers = warp::reply::with::headers(headers);
 
-	let mut headers = HeaderMap::new();
-	headers.insert("cache-control", HeaderValue::from_static("no-cache"));
-	let headers = warp::reply::with::headers(headers);
-	
-	let home = warp::path::end()
-		.map(|| homepage())
-		.map(|body| warp::reply::html(body)).with(&headers);
+    let home = warp::path::end()
+        .map(|| homepage())
+        .map(|body| warp::reply::html(body))
+        .with(&headers);
 
-	let routes = warp::get().and(home.or(warp::fs::dir("public")));
+    let routes = warp::get().and(home.or(warp::fs::dir("public")));
 
-	warp::serve(routes).run(([0, 0, 0, 0], 8080)).await;
+    warp::serve(routes).run(([0, 0, 0, 0], 8080)).await;
 }
 
 fn homepage() -> String {
-	String::from(html! {
-		(DOCTYPE)
-		html {
-			head {
-				title { "_" }
-				meta name="viewport" content="width=device-width, initial-scale=1";
-				link rel="stylesheet" type="text/css" href="global.css";
-			}
-			body {
-				video-waste {
-					template shadowrootmode="open" {
-						video autoplay="true" loop="true" muted="true" playsinline="true" {
-							source src="/video.mp4" type="video/mp4";
-						}
-						div {
-							.left {
-								canvas class="one" {}
-							}
-							.right {
-								canvas class="two" {}
-							}
-						}
-						script src="/video-waste.js" {}
-						link rel="stylesheet" type="text/css" href="video-waste.css";
-					}
-				}
-			}
-		}
-	})
+    String::from(html! {
+        (DOCTYPE)
+        html {
+            head {
+                title { "_" }
+                meta name="viewport" content="width=device-width, initial-scale=1";
+                link rel="stylesheet" type="text/css" href="global.css";
+            }
+            body {
+                video-waste {
+                    template shadowrootmode="open" {
+                        video autoplay="true" loop="true" muted="true" playsinline="true" {
+                            source src="/video.mp4" type="video/mp4";
+                        }
+                        div {
+                            .left {
+                                canvas class="one" {}
+                            }
+                            .right {
+                                canvas class="two" {}
+                            }
+                        }
+                        script src="/video-waste.js" {}
+                        link rel="stylesheet" type="text/css" href="video-waste.css";
+                    }
+                }
+            }
+        }
+    })
 }
